189813198eb53f06f402e28cf34c162f53caa7a2
JottaOS <juanmendozasfa@gmail.com>
2025-08-14 23:27:32 -0300

    fix cronjob syntax

--- src/scheduler/index.ts
+++ src/scheduler/index.ts
@@ -1,28 +1,23 @@
 import cron from "node-cron";
-import { insertActivityWithNotifications } from "../services/activities";
-import { formatScrapedActivities } from "../libs/utils";
-import { scrapedMock } from "../libs/constants";
+import {
+  getUpcomingActivities,
+  insertActivityWithNotifications,
+} from "../services/activities";
 
-const EVERY_SIX_HOURS = "* * */6 * * *";
-const EVERY_FIVE_MINUTES = "* */5 * * * *";
+const EVERY_SIX_HOURS = "0 */6 * * *";
+const EVERY_FIVE_MINUTES = "*/5 * * * *";
 
 const testEvery10Seconds = "*/10 * * * * *";
 
 console.log("[SYSTEM] Scheduler initialized");
 
-cron.schedule(testEvery10Seconds, async () => {
+cron.schedule(EVERY_SIX_HOURS, async () => {
   console.log(
     "[SYSTEM] Running automating scraping process",
     new Date().toISOString()
   );
   try {
-    // const upcomingActivities = await getUpcomingActivities();
-
-    const activities = formatScrapedActivities(scrapedMock);
-
-    const upcomingActivities = activities.filter(
-      (activity) => new Date(activity.closing_timestamp) >= new Date()
-    );
+    const upcomingActivities = await getUpcomingActivities();
 
     for (const activity of upcomingActivities) {
       await insertActivityWithNotifications(activity);
